b871e1a2e28e6c1213e4d8c05aadfa7053e8c1c2
sonsabo <89694812+sonsabo@users.noreply.github.com>
2024-10-15 21:10:18 +0200

    refactor: remove unused code

--- src/decorators/CommonDecorators.ts
+++
@@ -1,5 +0,0 @@
-export { PreEnforce } from "./PreEnforce";
-export { PostEnforce } from "./PostEnforce";
-export { EnforceDropWhileDenied } from "./EnforceDropWhileDenied";
-export { EnforceRecoverableIfDenied } from "./EnforceRecoverableIfDenied";
-export { EnforceTillDenied } from "./EnforceTillDenied";
